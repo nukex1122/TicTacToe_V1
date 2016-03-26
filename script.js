4bd8333e7cac907b0653aba05c30b9330f496bde
nukex1122 <md.mohsin.hussain786@gmail.com>
2016-03-26 19:36:58 +0530

    Added Design Elements

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+$(document).ready(main());
 
+var chance=0;
+var j=0;
 
-$(document).ready(function(){
-	$("div").html("Hello World!");
-});
+function main(){
+	$(".square").click(function(){
+		if($(this).find(".A").hasClass("picked") === false) {
+			$(this).css("background-color",(chance%2)?"#dc685a":"#ecaf4f");
+			$(this).find(".A").addClass("picked");
+			chance++;
+		}
+
+	});
+
+	$(".square").hover(function(){
+		if($(this).find(".A").hasClass("picked") === false) {
+				$(this).find(".A").addClass((chance%2)?"X":"O");
+		}
+	},function(){
+		if($(this).find(".A").hasClass("picked") === false ){
+			$(this).find(".A").removeClass((chance%2)?"X":"O");
+		}
+	});
+		
+}
